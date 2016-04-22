b396c602e1fab19a8d63ca1c0f397b6cbb88f7d4
Tim Ruffles <timruffles@gmail.com>
2016-04-22 14:39:57 +0100

    got sparkline v1

--- exercises/custom-element/sparkline.js
+++ exercises/custom-element/sparkline.js
@@ -7,10 +7,35 @@ module.exports.render = function({
   color = "black",
   points = [],  
 }) {
+  // TODO remove stub
+  points = [{x:10, y: 20}, {x:20, y: 25}, {x: 80, y: 95}];
 
-  d3.select(el)
-    .text(points.join("d3"))
+  // function makeLine(points: Array<{x: number, y: number}>): svgPathString {}
+  const makeLine = d3.svg.line()
+    .x(({x}) => x)
+    .y(({y}) => y)
   
+
+  const update = d3.select(el)
+    .selectAll("svg")
+    .data([points])
+
+  update.enter()
+    .append("svg")
+    .append("path")
+
+  // update + enter
+  update.select("svg")
+        .attr({ width: width, height })
+  
+  update.select("path")
+        .attr("d", makeLine)
+        .attr({
+          fill: "none",
+          stroke: color,
+        })
+
+
 };
 
 
